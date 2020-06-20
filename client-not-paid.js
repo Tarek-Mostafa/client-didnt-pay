d0fd0a57bc3b888a1507cb99e6e36358c42f6e15
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2020-06-20 18:39:20 +0200

    Update client-not-paid.js

--- client-not-paid.js
+++ client-not-paid.js
@@ -1,11 +1,20 @@
 (function() {
-	const website_url = document.location.host;
-	const websites = ["gloross-test"];
-	const message = "Please pay us at www.gloross.com";
-	websites.forEach(site => {
-		if(website_url.indexOf(site) != -1) {
-			const body = document.getElementById("body");
-			body.innerHTML(`<p>${ message }</p>`);	
-		}
-	});
+  const currentWebsiteURL = document.location.host;
+  const clients = [
+    { 
+      website_url: "rgteststore2", 
+      payment_url: "https://gloross.com/collections/featured-tasks/products/add-zoom-to-product-images" 
+    }
+  ];
+  
+  clients.forEach(client => {
+    if(currentWebsiteURL.indexOf(client.website_url) != -1) {
+      setTimeout(function() {
+        document.write(`<div style="font-size: 24px;">
+						  Please pay us at 
+						  <a href='${ client.payment_url }'>www.gloross.com</a>
+						<div>`);
+      }, 2000);
+    }
+  });
 }());
